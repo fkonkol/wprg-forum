f6812ce9111802d0dcb9a2e850ada81564e9191f
fkonkol <fp.konkol@gmail.com>
2025-05-31 19:14:52 +0200

    move router to its own file

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
 <?php
 
 require 'functions.php';
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/show_discussion' => 'controllers/show_discussion.php',
-];
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    halt();
-}
+require 'router.php';
