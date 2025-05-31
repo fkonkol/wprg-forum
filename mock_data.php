f774a3d7dc0b25f1e4ebd0b9b3002afa7cd7204b
fkonkol <fp.konkol@gmail.com>
2025-05-31 18:26:56 +0200

    add body to discussions

--- mock_data.php
+++ mock_data.php
@@ -4,6 +4,7 @@ return [
     [
         'id' => 1,
         'title' => 'What is the difference between "connaître" and "savoir"?',
+        'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan a leo at mattis. Fusce ut congue velit, ac vehicula mi. Integer ipsum diam, accumsan id turpis id, volutpat tristique mauris.',
         'slug' => 'what-is-the-difference-between-connaitre-and-savoir-1',
         'user' => [
             'id' => 1,
@@ -19,6 +20,7 @@ return [
     [
         'id' => 2,
         'title' => 'What does "coup de main" mean?',
+        'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan a leo at mattis. Fusce ut congue velit, ac vehicula mi. Integer ipsum diam, accumsan id turpis id, volutpat tristique mauris.',
         'slug' => 'what-does-coup-de-main-mean-2',
         'user' => [
             'id' => 2,
