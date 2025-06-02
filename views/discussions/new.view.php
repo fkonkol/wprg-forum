dcfd743f70cf6b3fe916e4cd65ee38981cff0709
fkonkol <fp.konkol@gmail.com>
2025-06-02 23:07:56 +0200

    some form styles

--- views/discussions/new.view.php
+++ views/discussions/new.view.php
@@ -1,30 +1,31 @@
 <?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/navigation.php') ?>
 
-<h1>Create a new discussion</h1>
+<main class="container">
+    <form action="/create_discussion" method="POST" class="flow">
+        <h1>Create a new discussion</h1>
 
-<form action="/create_discussion" method="POST">
-    <p>
-        <label for="discussion_title">Title</label>
-        <input type="text" name="title" id="discussion_title">
-    </p>
+        <div>
+            <label for="discussion_title" class="visually-hidden">Title</label>
+            <input type="text" name="title" id="discussion_title" placeholder="Title">
+        </div>
 
-    <p>
-        <label for="discussion_body">Body</label>
-        <textarea name="body" id="discussion_body"></textarea>
-    </p>
+        <div>
+            <label for="discussion_body" class="visually-hidden">Body</label>
+            <textarea name="body" id="discussion_body" placeholder="Body" rows="4"></textarea>
+        </div>
 
-    <p>
-        <label for="discussion_category_id">Category</label>
-        <select name="category_id" id="discussion_category_id">
-            <option value="">Select a category</option>
-            <option value="1">French</option>
-            <option value="2">Spanish</option>
-        </select>
-    </p>
+        <div>
+            <label for="discussion_category_id" class="visually-hidden">Category</label>
+            <select class="button button--secondary button--neutral" name="category_id" id="discussion_category_id">
+                <option value="">Select a category</option>
+                <option value="1">French</option>
+                <option value="2">Spanish</option>
+            </select>
+        </div>
 
-    <p>
-        <button type="submit">Submit</button>
-    </p>
-</form>
+        <button type="submit" class="button button--primary button--blueberry">Submit</button>
+    </form>
+</main>
 
 <?php require base_path('views/partials/foot.php') ?>
