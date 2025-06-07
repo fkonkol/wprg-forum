2d710d2cf9ac8f1d903796ce936525fcfdedc9ea
fkonkol <fp.konkol@gmail.com>
2025-06-08 00:02:27 +0200

    move router class

--- router.php
+++ router.php
@@ -1,45 +1,5 @@
 <?php
 
-class Router
-{
-    private array $routes = [];
-
-    public function dispatch(string $method, string $uri)
-    {
-        foreach ($this->routes as $route) {
-            if ($route['method'] === strtoupper($method) && $route['uri'] === $uri) {
-                return require base_path($route['controller']);
-            }
-        }
-
-        halt();
-    }
-
-    public function addRoute(string $method, string $uri, string $controller)
-    {
-        $this->routes[] = [
-            'method' => $method, 
-            'uri' => $uri, 
-            'controller' => $controller
-        ];
-    }
-
-    public function get(string $uri, string $controller)
-    {
-        $this->addRoute('GET', $uri, $controller);
-    }
-
-    public function post(string $uri, string $controller)
-    {
-        $this->addRoute('POST', $uri, $controller);
-    }
-
-    public function delete(string $uri, string $controller)
-    {
-        $this->addRoute('DELETE', $uri, $controller);
-    }
-}
-
 $router = new Router();
 
 $router->get('/', 'controllers/discussions/index.php');
